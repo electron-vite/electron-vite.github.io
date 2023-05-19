28261d7b6bc26e7998c3f3b78c40b5705bb01a9e
草鞋没号 <308487730@qq.com>
2023-05-19 15:28:00 +0800

    chore: style adjust

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,13 @@
 import Theme from 'vitepress/theme'
+
 import './styles/vars.css'
 
 export default {
   ...Theme,
+  enhanceApp({ app, router, siteData }) {
+    console.log(app)
+    console.log(router)
+    console.log(siteData)
+    // ...
+  }
 }
